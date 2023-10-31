f8440c69b99ffb25ba31a2ef136268b5292e5204
Emilio-Bueno <emilio__bueno@outlook.com>
2023-10-31 18:18:11 -0300

    Script Sala adicionado

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO TBL_PROFESSOR(nome,email, telefone, genero) VALUES('Emilio do Bom','emilio@gmail.com','11952395230','homi');
 INSERT INTO TBL_ADMIN(senha, email) VALUES('11952395230','homi');
+INSERT INTO TBL_SALA(nomedasala,tipo,capacidade,ndm,descricao) VALUES('Laborátorio 6','Laborátorio','85 alunos','30', 'Laborátorio com 30 máquinas e capacidade de 85 alunos');
